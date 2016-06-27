f103108cff695e3884bd13d933abb6d7a36b759c
jeffryporter <jporter7@gmail.com>
2016-06-27 12:28:44 -0400

    Add Upload funtionality and display capability

---
+++ src/main/java/com/ironyard/services/AnonFIleRepository.java
@@ -0,0 +1,17 @@
+package com.ironyard.services;
+
+import com.ironyard.entities.AnonFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jeffryporter on 6/27/16.
+ */
+
+public interface AnonFileRepository extends CrudRepository<AnonFile, Integer>
+{
+
+
+
+}
